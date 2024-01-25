0af9ccc1981e226e02352544c89ca2784ef4e52e
Sowmya Badana <badanasowmya555@gmail.com>
2024-01-25 10:58:40 -0500

    code refactoring

--- JS/script1.js
+++ JS/script1.js
@@ -2,43 +2,28 @@ var TxtType = function(el, toRotate, period) {
 this.toRotate = toRotate;
 this.el = el;
 this.loopNum = 0;
-this.period = parseInt(period, 10) || 2000;
 this.txt = '';
 this.tick();
-this.isDeleting = false;
 };
 
 TxtType.prototype.tick = function() {
     var i = this.loopNum % this.toRotate.length;
     var fullTxt = this.toRotate[i];
 
-    if (this.isDeleting) {
-        // this.txt = fullTxt.substring(0, this.txt.length - 1);
-        // delta=10
-    } else {
-        this.txt = fullTxt.substring(0, this.txt.length + 1);
-    }
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
 
     this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
 
     var that = this;
-    var delta = 200 - Math.random() * 100;
+    var delta = 150  
 
-    if (this.isDeleting) { delta /= 2; }
-
-    if (!this.isDeleting && this.txt === fullTxt) {
-        delta = this.period;
-        this.isDeleting = true;
-        // delta=10
-    } else if (this.isDeleting && this.txt === '') {
-        this.isDeleting = false;
-        this.loopNum++;
-        delta = 100;
-    }
+    if (this.txt.endsWith('.') || this.txt.endsWith('?')) {
+        delta=600
+    } 
 
     setTimeout(function() {
     that.tick();
-    }, 75);
+    }, delta);
 };
 
 window.onload = function() {
